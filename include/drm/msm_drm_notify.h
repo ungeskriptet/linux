028a4d239638318c4f2301208e1c8d676992f165
David Wronek <w.david0@protonmail.com>
2022-08-27 11:55:20 +0200

    input: Add NovaTek NT36672C SPI touchscreen driver
    Add the downstream driver for NT36672C by NovaTek.
    
    Signed-off-by: David Wronek <w.david0@protonmail.com>

---
+++ include/drm/msm_drm_notify.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2017, The Linux Foundation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _MSM_DRM_NOTIFY_H_
+#define _MSM_DRM_NOTIFY_H_
+
+#include <linux/notifier.h>
+
+/* A hardware display blank change occurred */
+#define MSM_DRM_EVENT_BLANK			0x01
+/* A hardware display blank early change occurred */
+#define MSM_DRM_EARLY_EVENT_BLANK		0x02
+
+#define DRM_EARLY_EVENT_BLANK   0x01
+#define DRM_EVENT_BLANK         0x02
+
+//enum {
+//	/* panel: power on */
+//	MSM_DRM_BLANK_UNBLANK,
+//	/* panel: power off */
+//	MSM_DRM_BLANK_POWERDOWN,
+//};
+
+enum {
+	DRM_BLANK_UNBLANK	= 1,
+	DRM_BLANK_LP1		= 2,
+	DRM_BLANK_LP2		= 3,
+	DRM_BLANK_STANDBY	= 4,
+	DRM_BLANK_SUSPEND	= 5,
+	DRM_BLANK_POWERDOWN	= 6,
+};
+
+enum msm_drm_display_id {
+	/* primary display */
+	MSM_DRM_PRIMARY_DISPLAY,
+	/* external display */
+	MSM_DRM_EXTERNAL_DISPLAY,
+	MSM_DRM_DISPLAY_MAX
+};
+
+struct msm_drm_notifier {
+	enum msm_drm_display_id id;
+	int data;
+};
+
+struct drm_notify_data {
+	bool is_primary;
+	int data;
+};
+
+int msm_drm_register_client(struct notifier_block *nb);
+int msm_drm_unregister_client(struct notifier_block *nb);
+int drm_register_client(struct notifier_block *nb);
+int drm_unregister_client(struct notifier_block *nb);
+int msm_drm_notifier_call_chain(unsigned long val, void *v);
+#endif
