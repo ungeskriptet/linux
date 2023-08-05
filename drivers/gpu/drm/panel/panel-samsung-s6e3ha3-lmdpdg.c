821263f37019883b6f561dd43fbfd666c4fe4e3c
David Wronek <davidwronek@gmail.com>
2023-08-05 18:05:12 +0200

    sync-dual-dsi

--- drivers/gpu/drm/panel/panel-samsung-s6e3ha3-lmdpdg.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e3ha3-lmdpdg.c
@@ -18,12 +18,6 @@
 
 #define RESET 1
 
-#define mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, cmd, seq...)        \
-		do {                                                 \
-			mipi_dsi_dcs_write_seq(dsi0, cmd, seq);      \
-			mipi_dsi_dcs_write_seq(dsi1, cmd, seq);      \
-		} while (0)
-
 struct s6e3ha3 {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi[2];
@@ -53,45 +47,45 @@ static void s6e3ha3_reset(struct s6e3ha3 *ctx)
 
 static int s6e3ha3_on(struct s6e3ha3 *ctx)
 {
-	struct mipi_dsi_device *dsi0 = ctx->dsi[0];
+	struct mipi_dsi_device *dsi = ctx->dsi[0];
 	struct mipi_dsi_device *dsi1 = ctx->dsi[1];
 
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x11);
+	mipi_dsi_dcs_write_seq(dsi, 0x11);
 	usleep_range(5000, 6000);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf0, 0x5a, 0x5a);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xc4, 0x03);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf9, 0x03);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xc2,
+	mipi_dsi_dcs_write_seq(dsi, 0xf0, 0x5a, 0x5a);
+	mipi_dsi_dcs_write_seq(dsi, 0xc4, 0x03);
+	mipi_dsi_dcs_write_seq(dsi, 0xf9, 0x03);
+	mipi_dsi_dcs_write_seq(dsi, 0xc2,
 			       0x00, 0x08, 0xd8, 0xd8, 0x00, 0x80, 0x2b, 0x05,
 			       0x08, 0x0e, 0x07, 0x0b, 0x05, 0x0d, 0x0a, 0x15,
 			       0x13, 0x20, 0x1e);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xed, 0x45);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf6,
+	mipi_dsi_dcs_write_seq(dsi, 0xed, 0x45);
+	mipi_dsi_dcs_write_seq(dsi, 0xf6,
 			       0x42, 0x57, 0x37, 0x00, 0xaa, 0xcc, 0xd0, 0x00,
 			       0x00);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf0, 0xa5, 0xa5);
+	mipi_dsi_dcs_write_seq(dsi, 0xf0, 0xa5, 0xa5);
 	msleep(120);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x12);
+	mipi_dsi_dcs_write_seq(dsi, 0x12);
 	usleep_range(1000, 2000);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x35, 0x00);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x53, 0x20);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x29);
+	mipi_dsi_dcs_write_seq(dsi, 0x35, 0x00);
+	mipi_dsi_dcs_write_seq(dsi, 0x53, 0x20);
+	mipi_dsi_dcs_write_seq(dsi, 0x29);
 	usleep_range(16000, 17000);
 
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x51, 0x1);
+	mipi_dsi_dcs_write_seq(dsi1, 0x51, 0x1);
 
 	return 0;
 }
 
 static int s6e3ha3_off(struct s6e3ha3 *ctx)
 {
-	struct mipi_dsi_device *dsi0 = ctx->dsi[0];
+	struct mipi_dsi_device *dsi = ctx->dsi[0];
 	struct mipi_dsi_device *dsi1 = ctx->dsi[1];
 
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x51, 0x0);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x28);
+	mipi_dsi_dcs_write_seq(dsi, 0x51, 0x0);
+	mipi_dsi_dcs_write_seq(dsi, 0x28);
 	msleep(50);
-	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x10);
+	mipi_dsi_dcs_write_seq(dsi, 0x10);
 	msleep(120);
 
 	return 0;
@@ -328,7 +322,6 @@ static int s6e3ha3_probe(struct mipi_dsi_device *dsi)
 static void s6e3ha3_remove(struct mipi_dsi_device *dsi)
 {
 	struct s6e3ha3 *ctx = mipi_dsi_get_drvdata(dsi);
-	int ret;
 
 	mipi_dsi_detach(ctx->dsi[0]);
 	mipi_dsi_detach(ctx->dsi[1]);
