f0a5ad5f861ada4f7d19df04f3c91b65f6c6bc5b
David Wronek <davidwronek@gmail.com>
2023-08-05 14:16:28 +0200

    Remove qcom,sync-dual-dsi

--- drivers/gpu/drm/panel/panel-samsung-s6e3ha3-lmdpdg.c
+++ drivers/gpu/drm/panel/panel-samsung-s6e3ha3-lmdpdg.c
@@ -6,16 +6,27 @@
 #include <linux/gpio/consumer.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/of_graph.h>
+
+#include <video/mipi_display.h>
 
 #include <drm/drm_mipi_dsi.h>
 #include <drm/drm_modes.h>
 #include <drm/drm_panel.h>
+#include <drm/drm_probe_helper.h>
 
-#define RESET 0
+#define RESET 1
+
+#define mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, cmd, seq...)        \
+		do {                                                 \
+			mipi_dsi_dcs_write_seq(dsi0, cmd, seq);      \
+			mipi_dsi_dcs_write_seq(dsi1, cmd, seq);      \
+		} while (0)
 
 struct s6e3ha3 {
 	struct drm_panel panel;
-	struct mipi_dsi_device *dsi;
+	struct mipi_dsi_device *dsi[2];
 #if RESET
 	struct gpio_desc *reset_gpio;
 #endif
@@ -42,41 +53,48 @@ static void s6e3ha3_reset(struct s6e3ha3 *ctx)
 
 static int s6e3ha3_on(struct s6e3ha3 *ctx)
 {
-	struct mipi_dsi_device *dsi = ctx->dsi;
+	struct mipi_dsi_device *dsi0 = ctx->dsi[0];
+	struct mipi_dsi_device *dsi1 = ctx->dsi[1];
 
-	mipi_dsi_dcs_write_seq(dsi, 0x11);
+	ctx->dsi[0]->mode_flags |= MIPI_DSI_MODE_LPM;
+	ctx->dsi[1]->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x11);
 	usleep_range(5000, 6000);
-	mipi_dsi_dcs_write_seq(dsi, 0xf0, 0x5a, 0x5a);
-	mipi_dsi_dcs_write_seq(dsi, 0xc4, 0x03);
-	mipi_dsi_dcs_write_seq(dsi, 0xf9, 0x03);
-	mipi_dsi_dcs_write_seq(dsi, 0xc2,
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf0, 0x5a, 0x5a);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xc4, 0x03);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf9, 0x03);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xc2,
 			       0x00, 0x08, 0xd8, 0xd8, 0x00, 0x80, 0x2b, 0x05,
 			       0x08, 0x0e, 0x07, 0x0b, 0x05, 0x0d, 0x0a, 0x15,
 			       0x13, 0x20, 0x1e);
-	mipi_dsi_dcs_write_seq(dsi, 0xed, 0x45);
-	mipi_dsi_dcs_write_seq(dsi, 0xf6,
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xed, 0x45);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf6,
 			       0x42, 0x57, 0x37, 0x00, 0xaa, 0xcc, 0xd0, 0x00,
 			       0x00);
-	mipi_dsi_dcs_write_seq(dsi, 0xf0, 0xa5, 0xa5);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0xf0, 0xa5, 0xa5);
 	msleep(120);
-	mipi_dsi_dcs_write_seq(dsi, 0x12);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x12);
 	usleep_range(1000, 2000);
-	mipi_dsi_dcs_write_seq(dsi, 0x35, 0x00);
-	mipi_dsi_dcs_write_seq(dsi, 0x53, 0x20);
-	mipi_dsi_dcs_write_seq(dsi, 0x29);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x35, 0x00);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x53, 0x20);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x29);
 	usleep_range(16000, 17000);
+
+	mipi_dsi_dcs_write_seq(dsi0, 0x51, 0x1);
 
 	return 0;
 }
 
 static int s6e3ha3_off(struct s6e3ha3 *ctx)
 {
-	struct mipi_dsi_device *dsi = ctx->dsi;
+	struct mipi_dsi_device *dsi0 = ctx->dsi[0];
+	struct mipi_dsi_device *dsi1 = ctx->dsi[1];
 
-	mipi_dsi_dcs_write_seq(dsi, 0x51, 0x00);
-	mipi_dsi_dcs_write_seq(dsi, 0x28);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x51, 0x0);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x28);
 	msleep(50);
-	mipi_dsi_dcs_write_seq(dsi, 0x10);
+	mipi_dsi_dual_dcs_write_seq(dsi0, dsi1, 0x10);
 	msleep(120);
 
 	return 0;
@@ -85,7 +103,8 @@ static int s6e3ha3_off(struct s6e3ha3 *ctx)
 static int s6e3ha3_prepare(struct drm_panel *panel)
 {
 	struct s6e3ha3 *ctx = to_s6e3ha3(panel);
-	struct device *dev = &ctx->dsi->dev;
+	struct mipi_dsi_device *dsi = ctx->dsi[0];
+	struct device *dev = &dsi->dev;
 	int ret;
 
 	if (ctx->prepared)
@@ -110,7 +129,7 @@ static int s6e3ha3_prepare(struct drm_panel *panel)
 static int s6e3ha3_unprepare(struct drm_panel *panel)
 {
 	struct s6e3ha3 *ctx = to_s6e3ha3(panel);
-	struct device *dev = &ctx->dsi->dev;
+	struct device *dev = &ctx->dsi[0]->dev;
 	int ret;
 
 	if (!ctx->prepared)
@@ -129,11 +148,11 @@ static int s6e3ha3_unprepare(struct drm_panel *panel)
 }
 
 static const struct drm_display_mode s6e3ha3_mode = {
-	.clock = (720 + 140 + 16 + 66) * (2560 + 16 + 16 + 8) * 60 / 1000,
-	.hdisplay = 720,
-	.hsync_start = 720 + 140,
-	.hsync_end = 720 + 140 + 16,
-	.htotal = 720 + 140 + 16 + 66,
+	.clock = (1440 + 280 + 32 + 132) * (2560 + 16 + 16 + 8) * 60 / 1000,
+	.hdisplay = 1440,
+	.hsync_start = 1440 + 280,
+	.hsync_end = 1440 + 280 + 32,
+	.htotal = 1440 + 280 + 32 + 132,
 	.vdisplay = 2560,
 	.vsync_start = 2560 + 16,
 	.vsync_end = 2560 + 16 + 16,
@@ -226,7 +245,9 @@ static int s6e3ha3_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
 	struct s6e3ha3 *ctx;
-	int ret;
+	struct mipi_dsi_host *dsi_sec_host;
+	struct device_node *dsi_sec;
+	int i, ret;
 
 	ctx = devm_kzalloc(dev, sizeof(*ctx), GFP_KERNEL);
 	if (!ctx)
@@ -239,13 +260,38 @@ static int s6e3ha3_probe(struct mipi_dsi_device *dsi)
 				     "Failed to get reset-gpios\n");
 #endif
 
-	ctx->dsi = dsi;
-	mipi_dsi_set_drvdata(dsi, ctx);
+	dsi_sec = of_graph_get_remote_node(dsi->dev.of_node, 1, -1);
 
-	dsi->lanes = 4;
-	dsi->format = MIPI_DSI_FMT_RGB888;
-	dsi->mode_flags = MIPI_DSI_MODE_NO_EOT_PACKET |
-			  MIPI_DSI_CLOCK_NON_CONTINUOUS;
+	if (dsi_sec) {
+		dev_notice(dev, "Using Dual-DSI\n");
+
+		const struct mipi_dsi_device_info info = { "S6E3HA3", 0,
+							   dsi_sec };
+
+		dev_notice(dev, "Found second DSI `%s`\n", dsi_sec->name);
+
+		dsi_sec_host = of_find_mipi_dsi_host_by_node(dsi_sec);
+		of_node_put(dsi_sec);
+		if (!dsi_sec_host) {
+			return dev_err_probe(dev, -EPROBE_DEFER,
+					     "Cannot get secondary DSI host\n");
+		}
+
+		ctx->dsi[1] =
+			mipi_dsi_device_register_full(dsi_sec_host, &info);
+		if (IS_ERR(ctx->dsi[1])) {
+			return dev_err_probe(dev, PTR_ERR(ctx->dsi[1]),
+					     "Cannot get secondary DSI node\n");
+		}
+
+		dev_notice(dev, "Second DSI name `%s`\n", ctx->dsi[1]->name);
+		mipi_dsi_set_drvdata(ctx->dsi[1], ctx);
+	} else {
+		dev_notice(dev, "Using Single-DSI\n");
+	}
+
+	ctx->dsi[0] = dsi;
+	mipi_dsi_set_drvdata(dsi, ctx);
 
 	drm_panel_init(&ctx->panel, dev, &s6e3ha3_panel_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
@@ -258,12 +304,26 @@ static int s6e3ha3_probe(struct mipi_dsi_device *dsi)
 
 	drm_panel_add(&ctx->panel);
 
-	ret = mipi_dsi_attach(dsi);
-	if (ret < 0) {
-		dev_err(dev, "Failed to attach to DSI host: %d\n", ret);
-		drm_panel_remove(&ctx->panel);
-		return ret;
+	for (i = 0; i < ARRAY_SIZE(ctx->dsi); i++) {
+		if (!ctx->dsi[i])
+			continue;
+
+		dev_notice(&ctx->dsi[i]->dev, "Binding DSI %d\n", i);
+
+		ctx->dsi[i]->lanes = 4;
+		ctx->dsi[i]->format = MIPI_DSI_FMT_RGB888;
+		ctx->dsi[i]->mode_flags = MIPI_DSI_MODE_NO_EOT_PACKET |
+						MIPI_DSI_CLOCK_NON_CONTINUOUS;
+
+		ret = mipi_dsi_attach(ctx->dsi[i]);
+		if (ret < 0) {
+			drm_panel_remove(&ctx->panel);
+			return dev_err_probe(dev, ret,
+					     "Failed to attach to DSI%d\n", i);
+		}
 	}
+
+	dev_notice(dev, "Panel probed successfully!\n");
 
 	return 0;
 }
@@ -273,9 +333,8 @@ static void s6e3ha3_remove(struct mipi_dsi_device *dsi)
 	struct s6e3ha3 *ctx = mipi_dsi_get_drvdata(dsi);
 	int ret;
 
-	ret = mipi_dsi_detach(dsi);
-	if (ret < 0)
-		dev_err(&dsi->dev, "Failed to detach from DSI host: %d\n", ret);
+	mipi_dsi_detach(ctx->dsi[0]);
+	mipi_dsi_detach(ctx->dsi[1]);
 
 	drm_panel_remove(&ctx->panel);
 }
