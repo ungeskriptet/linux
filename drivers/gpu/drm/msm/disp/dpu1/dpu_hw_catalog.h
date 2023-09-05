6fb1807cb4732150cccdab299524d005a881801c
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-09-05 20:43:52 +0300

    drm/msm/dpu: add support for SDM660 and SDM630 platforms
    Bring in hardware support for the SDM660 and SDM630 platforms, which
    belong to the same DPU generation as MSM8998.
    
    Note, by default these platforms are still handled by the MDP5 driver
    unless the `msm.prefer_mdp5=false' parameter is provided.
    
    Co-developed-by: Konrad Dybcio <konrad.dybcio@somainline.org>
    Signed-off-by: Konrad Dybcio <konrad.dybcio@somainline.org>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
+++ drivers/gpu/drm/msm/disp/dpu1/dpu_hw_catalog.h
@@ -822,6 +822,8 @@ struct dpu_mdss_cfg {
 };
 
 extern const struct dpu_mdss_cfg dpu_msm8998_cfg;
+extern const struct dpu_mdss_cfg dpu_sdm630_cfg;
+extern const struct dpu_mdss_cfg dpu_sdm660_cfg;
 extern const struct dpu_mdss_cfg dpu_sdm845_cfg;
 extern const struct dpu_mdss_cfg dpu_sm8150_cfg;
 extern const struct dpu_mdss_cfg dpu_sc8180x_cfg;
