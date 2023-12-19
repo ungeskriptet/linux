54b13f2176ff67c2f1fe573e776168a02958e4e6
Changbin Du <changbin.du@huawei.com>
2023-12-19 22:12:31 +0800

    modules: wait do_free_init correctly
    Commit 1a7b7d922081 ("modules: Use vmalloc special flag") moves
    do_free_init() into a global workqueue instead of call_rcu().  So now we
    should wait it via flush_work().
    
    Link: https://lkml.kernel.org/r/20231219141231.2218215-1-changbin.du@huawei.com
    Fixes: 1a7b7d922081 ("modules: Use vmalloc special flag")
    Signed-off-by: Changbin Du <changbin.du@huawei.com>
    Cc: Xiaoyi Su <suxiaoyi@huawei.com>
    Cc: Hui Wang <hw.huiwang@huawei.com>
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/moduleloader.h
+++ include/linux/moduleloader.h
@@ -115,6 +115,8 @@ int module_finalize(const Elf_Ehdr *hdr,
 		    const Elf_Shdr *sechdrs,
 		    struct module *mod);
 
+void flush_module_init_free_work(void);
+
 /* Any cleanup needed when module leaves. */
 void module_arch_cleanup(struct module *mod);
 
